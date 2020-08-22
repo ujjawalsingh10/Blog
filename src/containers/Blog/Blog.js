617b6c3b2c1d0c67e45ce2874edbf301d8ae0117
Ujjawal Singh <iamujjawal10.us@gmail.com>
2020-08-22 20:22:03 +0530

    AJAX /http

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount () {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                const posts = response.data.slice(0,4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author : 'Max'
+                    }
+                });
+                this.setState({ posts : updatedPosts});
+                // console.log(response);
+            });
+    }
+    render () {
+        const posts = this.state.posts.map(post =>{
+            return <Post 
+                title={post.title} 
+                author={post.author}
+                key={post.id}/>
+        });
+
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                    <Post />
+                    <Post />
+                </section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
