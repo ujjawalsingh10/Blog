d4d4d3e7bd5d4112477a90e88eed7e9ba1544261
Ujjawal Singh <iamujjawal10.us@gmail.com>
2020-08-23 10:51:21 +0530

    Adding interceptors

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,11 +10,12 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error : false
     }
 
     componentDidMount () {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('posts')
             .then(response => {
                 const posts = response.data.slice(0,4);
                 const updatedPosts = posts.map(post => {
@@ -25,7 +26,10 @@ class Blog extends Component {
                 });
                 this.setState({ posts : updatedPosts});
                 // console.log(response);
-            });
+            })
+            .catch(error => {
+                this.setState({error : true });
+            })
     }
 
     postSelectedHandler = (id) => {
@@ -33,14 +37,18 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post =>{
-            return <Post 
-                title={post.title} 
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id) }
-                key={post.id}/>
-        });
-
+        let posts = <p style={{textAlign : 'center'}}>Something went wrong</p>
+        if(!this.state.error){
+            posts = this.state.posts.map(post =>{
+                return <Post 
+                    title={post.title} 
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id) }
+                    key={post.id}/>
+            });
+    
+        }
+        
         return (
             <div>
                 <section className="Posts">
